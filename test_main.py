ddf2c48195cb02c228735d992a57a084b9fc8805
Voneska <91376307+Voneska@users.noreply.github.com>
2025-04-20 15:55:55 +0300

    add error

--- test_main.py
+++ test_main.py
@@ -11,7 +11,7 @@ def test_bitnumb_to_decimal_negative_number():
 
 
 def test_bitnumb_to_decimal_some_numbers():
-    assert bitnumb_to_decimal('1111111111110000') == -16
+    assert bitnumb_to_decimal('1111111111110001') == -16 # add error (1111111111110000)
     assert bitnumb_to_decimal('1000000000000001') == -32767
     assert bitnumb_to_decimal('0011001100110100 ') == 13108
     assert bitnumb_to_decimal('0101010101010101 ') == 21845
